bdc857ed9f3f607dd9ce9a54f3442c802b4d62f5
Sam Stevens <sam@ctrl-alt-sam.com>
2016-11-25 13:57:11 -0600

    eat dot increase score

--- NewDotGame/GameScene.swift
+++ NewDotGame/GameScene.swift
@@ -12,17 +12,20 @@ import SpriteKit
 class GameScene: SKScene {
     
     var counter = 0 // testing reasons
-    
     var score: Float = 0.1 // // HeadUpDisplay
    
     // Persist the initial touch position of the remote
     var touchPositionX: CGFloat = 0.0
     var touchPositionY: CGFloat = 0.0
-    let sprite = SKSpriteNode(imageNamed: "orangeCircle")
+    let dotSpeed: CGFloat = 1.5
+    
+    let playerDot = SKSpriteNode(imageNamed: "orangeCircle")
+    let smallDot = SKSpriteNode(imageNamed: "orangeCircle")
+    
     
     let labelScore = SKLabelNode(fontNamed: "ArialMT") // label to display the score
     
-    override func didMoveToView(view: SKView) {
+    override func didMove(to view: SKView) {
         /* Setup your scene here */
         
         //drawCircle()
@@ -32,34 +35,30 @@ class GameScene: SKScene {
         
         drawSprite()
         
-        sprite.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        self.addChild(sprite)
+        playerDot.position = CGPoint(x:self.frame.midX, y:self.frame.midY)
+        self.addChild(playerDot)
         
         
-        let tap = UITapGestureRecognizer(target: self, action: "tapped:")
-        tap.allowedPressTypes = [NSNumber(integer: UIPressType.Select.rawValue)]
+        let tap = UITapGestureRecognizer(target: self, action: #selector(GameScene.tapped(_:)))
+        tap.allowedPressTypes = [NSNumber(value: UIPressType.select.rawValue as Int)]
         self.view?.addGestureRecognizer(tap)
         
         createSmallDot()
         
-        // Todo List
-        // 1. a small dot(10px 10px) - Completed
-        // 2. Detect a collision between player dot and dot on the board
-        // 3. Clear a dot once it is hit - Currently working
-        // 4. Add it to score - You get a 0.1 points everytime you hit a dot
+       
 
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            touchPositionX = touch.locationInNode(self).x
-            touchPositionY = touch.locationInNode(self).y
+            touchPositionX = touch.location(in: self).x
+            touchPositionY = touch.location(in: self).y
         }
     }
     
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.locationInNode(self)
+            let location = touch.location(in: self)
             
             if touchPositionX != 0.0 && touchPositionY != 0.0 {
                 
@@ -68,10 +67,10 @@ class GameScene: SKScene {
                 let deltaY = touchPositionY - location.y
                 
                 // Calculate the new Sprite position
-                var x = sprite.position.x - deltaX
-                var y = sprite.position.y - deltaY
+                var x = playerDot.position.x - deltaX
+                var y = playerDot.position.y - deltaY
                 
-                // Check if the sprite will leave the screen
+                // Check if the playerDot will leave the screen
                 if x < 0 {
                     x = 0
                 } else if x > self.frame.width {
@@ -82,8 +81,8 @@ class GameScene: SKScene {
                 } else if y > self.frame.height {
                     y = self.frame.height
                 }
-                // Move the sprite
-                sprite.position = CGPoint(x: x, y: y)
+                // Move the playerDot
+                playerDot.position = CGPoint(x: x, y: y)
                 
             }
             // Persist latest touch position
@@ -92,42 +91,99 @@ class GameScene: SKScene {
         }
     }
    
-    override func update(currentTime: CFTimeInterval) {
+    func moveDot(_ deltaX: CGFloat, deltaY: CGFloat) {
+        
+        var x = playerDot.position.x + (deltaX * dotSpeed)
+        var y = playerDot.position.y + (deltaY * dotSpeed)
+        
+        // Check if the playerDot will leave the screen
+        if x < 0 {
+            x = 0
+        } else if x > self.frame.width {
+            x = self.frame.width
+        }
+        if y < 0 {
+            y = 0
+        } else if y > self.frame.height {
+            y = self.frame.height
+        }
+        // Move the playerDot
+        playerDot.position = CGPoint(x: x, y: y)
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
+        
         
         //Update the score
         updateHUD()
         
-        // Resize the sprite depending on the score
+        // Resize the playerDot depending on the score
         drawSprite()
         
         // Looking for a collision between two dots
+        if(playerDot.position.y <= smallDot.position.y){
+            //print("playerDot is below or equal to smallDot")
+        }
+        if(playerDot.position.x <= smallDot.position.x){
+            //print("yay")
+        }
+        
+        var Ay = smallDot.position.y+smallDot.size.height
+        var Bx = smallDot.position.x+smallDot.size.width
+        var Cy = smallDot.position.y-smallDot.size.height
+        var Dx = smallDot.position.x-smallDot.size.width
+        
+        if(playerDot.position.x <= Bx && playerDot.position.x >= Dx && playerDot.position.y <= Ay && playerDot.position.y >= Cy){
+            //print("playerDot ate smallDot")
+            
+            // move smallDot to random point after eaten
+            var Ry = Int(arc4random_uniform(100) + 50)
+            if(arc4random_uniform(2) == 0){
+                Ry *= -1
+            }
+            var Rx = Int(arc4random_uniform(100) + 50)
+            if(arc4random_uniform(2) == 0){
+                Rx *= -1
+            }
+            smallDot.position.x += CGFloat(Rx)
+            smallDot.position.y += CGFloat(Ry)
+            
+            
+            // make score increase
+            score = score + 0.1
+            print(score)
+            // erase smallDot
+        }
+        
+
         
     }
     
     func createSmallDot(){
         // create a dot that is 10px by 10px and is yellow
-        let smallDot = SKSpriteNode(imageNamed: "orangeCircle")
-        smallDot.position = CGPointMake(517, 400)
+        
+        smallDot.position = CGPoint(x: 517, y: 400)
         smallDot.size = CGSize(width: 10, height: 10)
         self.addChild(smallDot)
     }
     
     func drawSprite(){
-        sprite.yScale = CGFloat(score)
-        sprite.xScale = CGFloat(score)
+        playerDot.yScale = CGFloat(score)
+        playerDot.xScale = CGFloat(score)
         
     }
     
     
-    func tapped(gesture: UITapGestureRecognizer) { // touchpad tapped add 0.1 to the score
+    func tapped(_ gesture: UITapGestureRecognizer) { // touchpad tapped add 0.1 to the score
         score = score + 0.1
     }
     
     func createHUD(){ // Creates the scoreboard in the upper left corner
         
         labelScore.fontSize = 35;
-        labelScore.fontColor = .whiteColor()
+        labelScore.fontColor = SKColor.white
         labelScore.position = CGPoint(x:frame.minX + 100, y:frame.maxY - 100)
         addChild(labelScore)
         
@@ -141,7 +197,7 @@ class GameScene: SKScene {
     
     func drawCircle(){
         let bg = SKSpriteNode(imageNamed: "orangeCircle")
-        bg.position = CGPointMake(517, 400)
+        bg.position = CGPoint(x: 517, y: 400)
         bg.size = CGSize(width: 100, height: 100)
         self.addChild(bg)
         
@@ -152,8 +208,8 @@ class GameScene: SKScene {
                                       endAngle:CGFloat(M_PI * 2),
                                       clockwise: true)
         let shapeLayer = CAShapeLayer()
-        shapeLayer.path = circlePath.CGPath
-        shapeLayer.fillColor = UIColor.redColor().CGColor
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = UIColor.red.cgColor
         
     
     }
